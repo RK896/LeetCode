eb7335511cc907d6bcdb4c37580f3d23b808401b
rohit <rohit.karnik@outlook.com>
2025-04-02 18:51:52 -0400

    two_sum using two pointers approach

---
+++ arrays_and_hashing/two_sum.py
@@ -0,0 +1,26 @@
+from typing import List
+class Solution:
+    def twoSum_two_pointers(self, nums: List[int], target: int) -> List[int]:
+        left = 0
+        right = len(nums)-1
+
+        indexed_list = []
+
+        for i,num in enumerate(nums):
+            indexed_list.append((num, i))
+        indexed_list.sort()
+
+        while left < right:
+            sum = indexed_list[left][0] + indexed_list[right][0]
+
+            if sum == target:
+                return [indexed_list[left][1], indexed_list[right][1]]
+            if sum < target:
+                left+=1
+            elif sum > target:
+                right-=1
+        
+        return [0,0]
+
+sol = Solution()
+print(sol.twoSum_two_pointers([2,7,11,15], 9))
