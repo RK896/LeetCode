5775ac9718f299d9366c2f8316768ecb5d1fc491
rohit <rohit.karnik@outlook.com>
2025-04-04 19:26:31 -0400

    learned how to use bucket sort for this problem

--- arrays_and_hashing/k_most_frequent.py
+++ arrays_and_hashing/k_most_frequent.py
@@ -15,7 +15,27 @@ class Solution:
         for n in range(k):
             output.append(arr[len(arr)-n-1][1])
         return output
+    
+    def topKFrequent_bucket_sort(self, nums: List[int], k:int) -> List[int]:
+        count = {}
+        buckets = [[] for i in range(len(nums)+1)]
+
+        for x in nums: 
+            count[x] = count.get(x,0) + 1
+        
+        for num, counts in count.items():
+            buckets[counts].append(num)
+        
+        result = []
+        for i in range(len(buckets)-1, 0, -1):
+            for n in buckets[i]:
+                result.append(n)
+                if len(result) == k:
+                    return result
+
+
+
 
 sol = Solution()
-print(sol.topKFrequent_brute_force([1,1,1,2,2,3], 2))
-print(sol.topKFrequent_brute_force([1], 1))
+print(sol.topKFrequent_bucket_sort([1,1,1,2,2,3], 2))
+print(sol.topKFrequent_bucket_sort([1], 1))
